ea37aca3fe27cf72444b98addc416d9cbc709331
jnnzz <larocojanlorenz@gmail.com>
2025-03-09 00:00:58 +0800

    added form3

---
+++ AppsDevShare/WinFormsApp1/Form3.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            panel1 = new Panel();
+            panel3 = new Panel();
+            panel2 = new Panel();
+            pictureBox3 = new PictureBox();
+            Company = new Label();
+            BExit = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(220, 223, 231);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(0, -4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1052, 732);
+            panel1.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(58, 94, 116);
+            panel3.Location = new Point(67, 212);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(904, 445);
+            panel3.TabIndex = 11;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(58, 94, 113);
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(pictureBox3);
+            panel2.Controls.Add(Company);
+            panel2.Controls.Add(BExit);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1052, 35);
+            panel2.TabIndex = 3;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(3, 1);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(42, 30);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 10;
+            pictureBox3.TabStop = false;
+            // 
+            // Company
+            // 
+            Company.AutoSize = true;
+            Company.Font = new Font("Microsoft New Tai Lue", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Company.ForeColor = SystemColors.ButtonHighlight;
+            Company.Location = new Point(44, 9);
+            Company.Name = "Company";
+            Company.Size = new Size(82, 20);
+            Company.TabIndex = 9;
+            Company.Text = "MediTrack";
+            // 
+            // BExit
+            // 
+            BExit.BackgroundImage = (Image)resources.GetObject("BExit.BackgroundImage");
+            BExit.BackgroundImageLayout = ImageLayout.Zoom;
+            BExit.FlatAppearance.BorderSize = 0;
+            BExit.FlatStyle = FlatStyle.Flat;
+            BExit.Location = new Point(1014, 6);
+            BExit.Name = "BExit";
+            BExit.Size = new Size(20, 25);
+            BExit.TabIndex = 0;
+            BExit.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1049, 716);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click_1;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(489, 41);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(71, 59);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 11;
+            pictureBox2.TabStop = false;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(220, 223, 231);
+            ClientSize = new Size(1045, 715);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form3";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form3";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Panel panel2;
+        private PictureBox pictureBox3;
+        private Label Company;
+        private Button BExit;
+        private Panel panel3;
+        private PictureBox pictureBox2;
+    }
+}
