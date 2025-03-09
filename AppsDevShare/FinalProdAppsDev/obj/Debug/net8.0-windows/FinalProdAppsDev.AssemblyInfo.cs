8251cadded566661dd55273d82a7c296415cfe36
jnnzz <larocojanlorenz@gmail.com>
2025-03-09 22:23:57 +0800

    added signUpPage

--- AppsDevShare/FinalProdAppsDev/obj/Debug/net8.0-windows/FinalProdAppsDev.AssemblyInfo.cs
+++ AppsDevShare/FinalProdAppsDev/obj/Debug/net8.0-windows/FinalProdAppsDev.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProdAppsDev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8b7d7a946dd7ef1a0b50bb6d2c1915192da0c39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c31027091862cba510e6313bda65bea2200dcc7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProdAppsDev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProdAppsDev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
