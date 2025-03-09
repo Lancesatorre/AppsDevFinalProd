249c4799d3965d2058d84e6dcd4f909552de7be7
jnnzz <larocojanlorenz@gmail.com>
2025-03-09 23:32:35 +0800

    added QRcodePage

--- AppsDevShare/FinalProdAppsDev/obj/Debug/net8.0-windows/FinalProdAppsDev.AssemblyInfo.cs
+++ AppsDevShare/FinalProdAppsDev/obj/Debug/net8.0-windows/FinalProdAppsDev.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProdAppsDev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0608dc5c5a2891e5aa5af81e9b95abe638491fc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6f87003eb541f4f4e0fc8b4588a027d2e3f0955")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProdAppsDev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProdAppsDev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
