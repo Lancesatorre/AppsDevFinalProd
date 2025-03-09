56c09d55a529985fcb7ce6cd79a57905eb55efb8
Lance Satorre <lanceerrotas@gmail.com>
2025-03-09 22:11:19 +0800

    Main Project

--- AppsDevShare/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ AppsDevShare/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ee93453f8025ee1feb3ea072f57fad8c0df12b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8b7d7a946dd7ef1a0b50bb6d2c1915192da0c39")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
